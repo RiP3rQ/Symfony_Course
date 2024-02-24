8d6ab56102b6627e79996b086314012958fce8dd
RiP3rQ <rafalpompa2000@gmail.com>
2024-02-25 00:31:21 +0100

    Non Verified Users Can't Post

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -41,7 +41,7 @@ class MicroPostController extends AbstractController
     }
 
     #[Route('/micro-post/add', name: 'app_micro_post_add', priority: 2)]
-    #[IsGranted('IS_AUTHENTICATED_FULLY')]
+    #[IsGranted('ROLE_WRITER')]
     public function add(Request $request, EntityManagerInterface $em): Response
     {
         $form = $this->createForm(MicroPostType::class, new MicroPost());
