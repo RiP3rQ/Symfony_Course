1891dfdec656340fe51cf8c955721f9a61b085cc
RiP3rQ <rafalpompa2000@gmail.com>
2024-02-24 19:02:47 +0100

    Styling + Editing Paths Adding

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -82,7 +82,7 @@ class MicroPostController extends AbstractController
         }
 
         return $this->render(
-            'micro_post/add.html.twig',
+            'micro_post/edit.html.twig',
             [
                 'form' => $form
             ]
