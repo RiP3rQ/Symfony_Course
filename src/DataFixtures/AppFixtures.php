d883722014e762645133b10141568da46ac76efe
RiP3rQ <rafalpompa2000@gmail.com>
2024-02-24 20:47:47 +0100

    Create user - custom command

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -7,10 +7,37 @@ use App\Entity\MicroPost;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 
+use App\Entity\User;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
 class AppFixtures extends Fixture
 {
+    public function __construct(
+        private UserPasswordHasherInterface $userPasswordHasher
+    ) {
+    }
     public function load(ObjectManager $manager): void
     {
+        $user1 = new User();
+        $user1->setEmail('test@test.com');
+        $user1->setPassword(
+            $this->userPasswordHasher->hashPassword(
+                $user1,
+                '12345678'
+            )
+        );
+        $manager->persist($user1);
+
+        $user2 = new User();
+        $user2->setEmail('john@test.com');
+        $user2->setPassword(
+            $this->userPasswordHasher->hashPassword(
+                $user2,
+                '12345678'
+            )
+        );
+        $manager->persist($user2);
+
         $microPost1 = new MicroPost();
         $microPost1->setTitle('Welcome to Poland!');
         $microPost1->setText('Welcome to Poland!');
@@ -28,6 +55,8 @@ class AppFixtures extends Fixture
         $microPost3->setText('Welcome to Germany!');
         $microPost3->setCreated(new DateTime());
         $manager->persist($microPost3);
+
+        // Activate via: symfony console doctrine:fixtures:load
 
         $manager->flush();
     }
